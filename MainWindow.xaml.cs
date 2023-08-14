6dfedf808851c04969fb7e8f9816f1d77c6fa2af
XeinTDM <mjaow2020@gmail.com>
2023-08-14 22:51:26 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using OnlyDox;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace  OnlyDox
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateToPage(UserControl page)
+        {
+            // Animation
+            DoubleAnimation fadeAnimation = new DoubleAnimation
+            {
+                From = 0.0,
+                To = 1.0,
+                Duration = new Duration(TimeSpan.FromSeconds(0.5))
+            };
+            page.BeginAnimation(UIElement.OpacityProperty, fadeAnimation);
+
+            ContentFrame.Content = page;
+        }
+
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(new HomePage());
+        }
+
+        private void OnlyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(new OnlyPage());
+        }
+
+        private void SettingsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(new SettingsPage());
+        }
+    }
+}
---
+++ SubtractValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace OnlyDox
+{
+    public class SubtractValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width && parameter is string offsetStr && double.TryParse(offsetStr, out double offset))
+            {
+                return width - offset;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
